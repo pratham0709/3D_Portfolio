6c52d2a37aa9cc99cb3e31eb176ac8e8d8d4971c
Prathamesh Jadhav <109234076+pratham0709@users.noreply.github.com>
2023-12-30 14:45:26 +0530

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -16,8 +16,7 @@ const renderContent = {
     1 : (
         <h1 className="sm:text-xl sm:leading-snug text-center
         neo-brutalism-blue py-4 px-8 text-white mx-5">
-        Hi, I am<span className="font-semibold mx-2 text-white">Prathamesh</span>
-        👋
+        Hi, I am<span className="font-semibold mx-2 text-white">Prathamesh👋</span>
         <br />
         A Frontend Developer From India
         </h1>
